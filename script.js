215538e412385c8ec5f6ab36c05d64c7f2e45ba3
Yonrheal Hearn <hyonrheal@yahoo.com>
2024-04-29 17:55:21 -0400

    final updates

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let holeCount = prompt("Would you like to play 3 or 6 holes?");
 let totalputs = 0
 let par = 0
 console.log(holeCount); 
+let total;
 if(holeCount==="3"){
 console.log("user selected 3 holes")
 let holeCount1 = prompt("How many putts for hole 1?");
@@ -10,6 +11,7 @@ let holeCount2 = prompt("How many putts for hole 2?");
 let holeCount3 = prompt("How many putts for hole 3?");
 par = 9
 totalputs = Number(holeCount1) + Number(holeCount2) + Number(holeCount3)
+total = par - totalputs
 } else if(holeCount==="6"){
 console.log("user selected 6")
 let holeCount1 = prompt("How many putts for hole 1?");
@@ -19,12 +21,16 @@ let holeCount4 = prompt("How many putts for hole 4?");
 let holeCount5 = prompt("How many putts for hole 5?");
 let holeCount6 = prompt("How many putts for hole 6?");
 par = 18
+totalputs = Number(holeCount1) + Number(holeCount2) + Number(holeCount3)
 }
 console.log(totalputs);
-if(totalputs===par){
-console.log("even with par")
+if (total ===0){
+console.log(`Good game, ${firstName}. Your total par was: 0.`)
+} else if (total < 0){
+console.log(`Great job, ${firstName} Your total par was: -(par)`)
+} else { (total > 0)
+console.log(`Nice try, ${firstname}... Your total par was: +(par)`)
 }
-
 // let holeCount1 = prompt("How many putts for hole 1?");
 // let holeCount2 = prompt("How many putts for hole 2?");
 // let holeCount3 = prompt("How many putts for hole 3?");
